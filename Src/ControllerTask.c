08d76c373fc319738fcd5df2143adee6c0829e40
marton bognar <martonbognar@sch.bme.hu>
2017-04-20 19:50:18 +0200

    Test controller frequency

--- Src/ControllerTask.c
+++ Src/ControllerTask.c
@@ -10,7 +10,7 @@ void StartController(void const * argument) {
 	const TickType_t xFrequency = 10 * portTICK_PERIOD_MS;
 
 	xLastWakeTime = xTaskGetTickCount();
-	// uint8_t cnt = 0;
+	uint8_t cnt = 0;
 	for(;;)
 	{
 		vTaskDelayUntil( &xLastWakeTime, xFrequency );
@@ -28,11 +28,11 @@ void StartController(void const * argument) {
 		  pushDebugMessage(test);
 */
 		// test calling frequency
-		/*
+
 		char buf[256];
 		sprintf(buf, "%d|", cnt++);
 		pushMessage(buf);
-		*/
+
 		HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_14);
 	}
 }
